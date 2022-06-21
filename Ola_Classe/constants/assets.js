e5676e8e587f8c8b13e2c9719527e93eba7678eb
Alexxmfs <alexxmfs@hotmail.com>
2022-06-21 17:03:39 -0300

    bang

--- Ola_Classe/constants/assets.js
+++ Ola_Classe/constants/assets.js
@@ -1,7 +1,6 @@
 import easyAccess from "../assets/image/easyAccess.png";
 import onlineSchool from "../assets/image/onlineSchool.png";
 import findCourse from "../assets/image/findCourse.png";
-import Line from "../assets/image/Line.png";
 import next from "../assets/icons/next.png";
 
 export default { 
@@ -9,7 +8,6 @@ export default {
 easyAccess,
 onlineSchool,
 findCourse,
-Line,
 next
 
 };
--- Ola_Classe/src/Screens/FirstIntroduction.js
+++ Ola_Classe/src/Screens/FirstIntroduction.js
@@ -5,21 +5,17 @@ import { CircleButton } from '../components/Button'
 
 const FirstIntroduction = (props) => {
   return (
-    <SafeAreaView>
-    <View style={{ width: "100%", alignItems: 'center', justifyContent: 'center' }}>
-    <Image 
-    source={assets.Line}
-    style={{ top: 312, width: "100%"}}
-    />
+    <View style={{ flex: 1, width: "100%", alignItems: 'center', justifyContent: 'center' }}>
+
     <Image
     source={assets.easyAccess}
-    style={{ width: 270, height: 290, top: 30 }}
+    style={{ width: 390, height: 290 }}
     />
 
-    <Text style={{
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+   <Text style={{
     fontFamily: FONTS.bold, 
     fontSize: SIZES.large,
-    top: 45
     }}>
     Fácil Acesso
     </Text>
@@ -27,19 +23,19 @@ const FirstIntroduction = (props) => {
     <Text style={{
     fontFamily: FONTS.regular, 
     fontSize: SIZES.font,
-    paddingTop: 65
     }}>
     Aprenda facilmente com Olá Classe!, você pode acessar muitos cursos e mentores.
     </Text>
-    <View style={{ left: 135, paddingTop: 70}}>
+    </View>
+    
+    <View style={{ flex: 1, left: 140 }}>
+ 
     <CircleButton 
-    style = {{ right: 150}}
     imgUrl = {assets.next}
     onPress={() => props.navigation.navigate("SecondIntroduction")}
     />
         </View>
     </View>
-    </SafeAreaView>
   )
 }
 
