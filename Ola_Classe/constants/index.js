3997931e04dcd79a886b76906ee022e643dc838c
Alexxmfs <alexxmfs@hotmail.com>
2022-08-24 19:00:18 -0300

    expo-av

--- Ola_Classe/constants/index.js
+++
@@ -1,5 +0,0 @@
-import assets from "./assets";
-import { COLORS, SIZES, FONTS, SHADOWS } from "./theme";
-import { Introduction } from "./dummy";
-
-export { assets, COLORS, SIZES, FONTS, SHADOWS, Introduction };
---
+++ VideoPlayer/App.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { Video, AVPlaybackStatus } from 'expo-av';
+
+export default function App() {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+  return (
+    <View style={styles.container}>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={require('./assets/videoEinerd.mp4')}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  video: {
+    alignSelf: 'center',
+    width: 320,
+    height: 200,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
