9fa346d3f7d494bc7e899af1d5312f6dc5541e1b
Alex <88356153+Alexxmfs@users.noreply.github.com>
2022-09-07 00:00:45 -0300

    link

--- Vitrine/src/components/Button.jsx
+++ Vitrine/src/components/Button.jsx
@@ -5,7 +5,7 @@ import styles from '../styles/Global';
 const Button = ({ assetUrl, Link }) => {
   return (
     <div className={styles.btnBlack}
-          onClick={() => window.open('https://www.w3schools.com', "_blank")}>
+          onClick={() => window.open('https://expo.dev/accounts/zprinted/projects/Ola_Classe/builds/71548221-c5dd-4def-a2d5-5031ffe57734', "_blank")}>
       <img src={assetUrl} alt="expo_icon" 
       className={styles.btnIcon} />
       <div className="flex flex-col justify-start ml-4">
