ee20b9274a46a805201c9a74458ca90a6a447ef7
Alexxmfs <alexxmfs@hotmail.com>
2022-10-03 20:21:58 -0300

    QUACK

--- Ola_Classe/src/Screens/Category_Languages/CategoryAngularScreen.js
+++ Ola_Classe/src/Screens/Category_Languages/CategoryAngularScreen.js
@@ -100,7 +100,7 @@ const CategoryAngularScreen = ({ navigation }) => {
             >
               <IconLike />
               <IconWarning />
-              <IconSave />
+              {/* <IconSave /> */}
               <IconComment />
             </View>
           </View>
@@ -171,7 +171,7 @@ const CategoryAngularScreen = ({ navigation }) => {
             >
               <IconLike />
               <IconWarning />
-              <IconSave />
+              {/* <IconSave /> */}
               <IconComment />
             </View>
           </View>
@@ -242,7 +242,7 @@ const CategoryAngularScreen = ({ navigation }) => {
             >
               <IconLike />
               <IconWarning />
-              <IconSave />
+              {/* <IconSave /> */}
               <IconComment />
             </View>
           </View>
